978ce9996365f187ca59c2f8a7d9432010125449
Satyaprakash Tiwari <satyaprakash4253@gmail.com>
2024-07-01 22:02:21 +0530

    basic functionalities

---
+++ gates.js
@@ -0,0 +1,233 @@
+import { nanoid } from "nanoid"
+
+class Gates {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+        this.height = 60
+        this.actualWidth = 120
+        this.width = 70
+        this.color1 = '#6965db'
+        // this.color2 = 
+        this.offset = {x: 0, y: 0}
+        this.r = 3
+        this.speed = 5
+        this.keys = new Set()
+        this.id = nanoid()
+    }
+
+    checkMouse(x, y) {
+        let xMax = this.x + this.width/2
+        let xMin = this.x - this.width/2
+        let yMax = this.y + this.height/2
+        let yMin = this.y - this.height/2
+
+        return (x < xMax && x > xMin) && (y < yMax && y > yMin)
+    }
+
+    checkSelector(s) {
+        let xMax = s.x + s.width
+        let xMin = s.x
+        let yMax = s.y + s.height
+        let yMin = s.y
+
+        return (this.x < xMax && this.x > xMin) && (this.y < yMax && this.y > yMin)
+    }
+
+    setContextProperty(ctx) {
+        ctx.lineWidth = 1
+        ctx.fillStyle = '#fff'
+        ctx.strokeStyle = '#000'
+    }
+
+    drawNodes21(ctx) {
+        let r = this.r
+        ctx.beginPath()
+        ctx.moveTo(this.x + this.width/2, this.y)
+        ctx.lineTo(this.x + this.actualWidth/2, this.y)
+        ctx.closePath()
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.moveTo(this.x - this.width/2, this.y - this.height/3.5)
+        ctx.lineTo(this.x - this.actualWidth/2, this.y - this.height/3.5)
+        ctx.closePath()
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.moveTo(this.x - this.width/2, this.y + this.height/3.5)
+        ctx.lineTo(this.x - this.actualWidth/2, this.y + this.height/3.5)
+        ctx.closePath()
+        ctx.stroke()
+
+        // circles
+        ctx.beginPath()
+        ctx.arc(this.x + this.actualWidth/2 - r, this.y, r, 0, Math.PI*2)
+        ctx.closePath()
+        ctx.fill()
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.arc(this.x - this.actualWidth/2 + r, this.y - this.height/3.5, r, 0, Math.PI*2)
+        ctx.closePath()
+        ctx.fill()
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.arc(this.x - this.actualWidth/2 + r, this.y + this.height/3.5, r, 0, Math.PI*2)
+        ctx.closePath()
+        ctx.fill()
+        ctx.stroke()
+    }
+
+    drawNodes11(ctx) {
+        let r = this.r
+        ctx.beginPath()
+        ctx.moveTo(this.x + this.width/2, this.y)
+        ctx.lineTo(this.x + this.actualWidth/2, this.y)
+        ctx.closePath()
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.moveTo(this.x - this.width/2, this.y)
+        ctx.lineTo(this.x - this.actualWidth/2, this.y)
+        ctx.closePath()
+        ctx.stroke()
+
+        //circles
+        ctx.beginPath()
+        ctx.arc(this.x + this.actualWidth/2 - r, this.y, r, 0, Math.PI*2)
+        ctx.closePath()
+        ctx.fill()
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.arc(this.x - this.actualWidth/2 + r, this.y, r, 0, Math.PI*2)
+        ctx.closePath()
+        ctx.fill()
+        ctx.stroke()
+    }
+
+    showBorder(ctx) {
+        if (!this.border) return
+        
+        let pad = 10
+        ctx.strokeStyle = '#6965db'
+        ctx.lineWidth = 2
+        ctx.fillStyle = '#fffb'
+        // ctx.strokeRect(this.x-this.actualWidth/2 - pad, this.y-this.height/2 - pad, this.actualWidth + 2*pad, this.height + 2*pad)
+        ctx.fillRect(this.x-this.actualWidth/2 - pad, this.y-this.height/2 - pad, this.actualWidth + 2*pad, this.height + 2*pad)
+    
+        ctx.beginPath()
+        ctx.moveTo(this.x-this.actualWidth/2 - pad, this.y - this.height/2 + pad)
+        ctx.lineTo(this.x-this.actualWidth/2 - pad, this.y - this.height/2 - pad)
+        ctx.lineTo(this.x-this.actualWidth/2 + pad, this.y - this.height/2 - pad)
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.moveTo(this.x+this.actualWidth/2 + pad, this.y - this.height/2 + pad)
+        ctx.lineTo(this.x+this.actualWidth/2 + pad, this.y - this.height/2 - pad)
+        ctx.lineTo(this.x+this.actualWidth/2 - pad, this.y - this.height/2 - pad)
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.moveTo(this.x-this.actualWidth/2 - pad, this.y + this.height/2 - pad)
+        ctx.lineTo(this.x-this.actualWidth/2 - pad, this.y + this.height/2 + pad)
+        ctx.lineTo(this.x-this.actualWidth/2 + pad, this.y + this.height/2 + pad)
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.moveTo(this.x+this.actualWidth/2 + pad, this.y + this.height/2 - pad)
+        ctx.lineTo(this.x+this.actualWidth/2 + pad, this.y + this.height/2 + pad)
+        ctx.lineTo(this.x+this.actualWidth/2 - pad, this.y + this.height/2 + pad)
+        ctx.stroke()
+
+    }
+
+    update() {
+        if (this.keys.has('LEFT')) this.x -= this.speed
+        if (this.keys.has('RIGHT')) this.x += this.speed
+        if (this.keys.has('UP')) this.y -= this.speed
+        if (this.keys.has('DOWN')) this.y += this.speed
+    }
+}
+
+export class AND extends Gates{
+    constructor(x, y) {
+        super(x, y)
+        this.name = 'AND'
+    }
+
+    draw(ctx) {
+        this.showBorder(ctx)
+        this.setContextProperty(ctx)
+        this.drawNodes21(ctx)
+
+        // ctx.strokeRect(this.x - this.actualWidth/2, this.y - this.height/2, this.actualWidth, this.height)
+        ctx.beginPath()
+        ctx.moveTo(this.x - this.width/2, this.y - this.height/2)
+        ctx.lineTo(this.x + this.width/2 - this.height/2, this.y - this.height/2)
+        ctx.arc(this.x + this.width/2 - this.height/2, this.y, this.height/2, -Math.PI/2, Math.PI/2)
+        ctx.lineTo(this.x - this.width/2, this.y + this.height/2)
+        ctx.lineTo(this.x - this.width/2, this.y - this.height/2)
+        ctx.closePath()
+        ctx.fill()
+        ctx.stroke()
+    }
+}
+
+
+export class OR extends Gates{
+    constructor(x, y) {
+        super(x, y)
+        this.name = 'OR'
+    }
+
+    draw(ctx) {
+        this.showBorder(ctx)
+        this.setContextProperty(ctx)
+        this.drawNodes21(ctx)
+
+        // ctx.strokeRect(this.x - this.actualWidth/2, this.y - this.height/2, this.actualWidth, this.height)
+        ctx.beginPath()
+        ctx.moveTo(this.x - this.width/1.8, this.y + this.height/2)
+        ctx.quadraticCurveTo(this.x - this.width/3 , this.y, this.x - this.width/1.8, this.y - this.height/2)
+        ctx.bezierCurveTo(
+            this.x + this.width/1.2, this.y - this.height/2.3, 
+            this.x + this.width/1.2, this.y + this.height/2.3,
+            this.x - this.width/1.8, this.y + this.height/2
+        )
+        ctx.fill()
+        ctx.stroke()
+    }
+}
+
+export class NOT extends Gates{
+    constructor(x, y) {
+        super(x, y)
+        this.name = 'NOT'
+        this.width = this.height
+        this.actualWidth = 110
+    }
+
+    draw(ctx) {
+        this.showBorder(ctx)
+        this.setContextProperty(ctx)
+        this.drawNodes11(ctx)
+
+        // ctx.strokeRect(this.x - this.actualWidth/2, this.y - this.height/2, this.actualWidth, this.height)
+        ctx.beginPath()
+        ctx.moveTo(this.x - this.width/2, this.y + this.height/2)
+        ctx.lineTo(this.x - this.width/2, this.y - this.height/2)
+        ctx.lineTo(this.x + this.width/2.5, this.y)
+        ctx.closePath()
+        ctx.fill()
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.arc(this.x + this.width/2, this.y, 5, 0, 2*Math.PI)
+        ctx.closePath()
+        ctx.fill()
+        ctx.stroke()
+    }
+}
---
+++ script.js
@@ -0,0 +1,251 @@
+import { AND, OR, NOT } from "./gates"
+
+let scaleFactor = 1
+let translate = {x: 0, y: 0}
+// let moveBtn = document.querySelector('.move-btn')
+// let moveBtnPressed = false
+// const flipMove = () => {
+//     moveBtnPressed = !moveBtnPressed
+//     moveBtn.classList.toggle('active')
+//     if (moveBtnPressed) {
+//         canvas.style.cursor = 'grab'
+//     } else {
+//         canvas.style.cursor = 'default'
+//     }
+// }
+// moveBtn.onclick = () => flipMove()
+
+
+let createPos = { x: null, y: null } // stores the initial position of gate being created
+let gates = [] // stores all the gates in the canvas
+gates.push(new AND(300, 400))
+gates.push(new NOT(600, 200))
+
+
+let contextMenu = document.querySelector('.context-menu')
+let gatesMenu = [
+    {name: 'AND', create: (pos) => gates.push(new AND(pos.x, pos.y))},
+    {name: 'OR', create: (pos) => gates.push(new OR(pos.x, pos.y))},
+    {name: 'NOT', create: (pos) => gates.push(new NOT(pos.x, pos.y))},
+    // {name: 'NAND'},
+    // {name: 'NOR'},
+    // {name: 'XOR'}
+]
+gatesMenu.forEach(menu => {
+    let option = document.createElement('div')
+    option.classList.add('option')
+    option.innerText = menu.name
+    option.onclick = (e) => {
+        menu.create({x: e.clientX - translate.x, y: e.clientY- translate.y})
+        hideContextMenu()
+    }
+    contextMenu.appendChild(option)
+})
+
+
+
+var canvas = document.querySelector('#canvas')
+let ctx = canvas.getContext('2d')
+canvas.width = window.innerWidth*window.devicePixelRatio
+canvas.height = window.innerHeight*window.devicePixelRatio
+
+
+
+let prev={x:0, y:0}
+let selector = {x: 0, y: 0, width: 0, height: 0}
+let selectedArray = []
+let mousePressed = false
+canvas.addEventListener('mousedown', (e) => { 
+    // animate() // rerender everytime the mouse is down
+    let x = e.clientX
+    let y = e.clientY
+    prev = {x, y}
+    selector.x = x
+    selector.y = y
+    mousePressed = true
+
+    // setting offset for multiple gates inside the selector
+    for (let i = 0; i < selectedArray.length; i++) {
+        let mouseInsideGate = selectedArray[i].checkMouse(x - translate.x, y - translate.y)
+        
+        // if mouse is inside one of the selected gate, offset each selected gate w.r.t x,y
+        if (mouseInsideGate) {
+            if (e.ctrlKey) { // if ctrl is pressend when clicking on the selected gate, remove it from the selectedArray
+                selectedArray[i].border = false
+                selectedArray.splice(i, 1)
+            }
+            else { // else offset each selected gate w.r.t x,y
+                selectedArray.forEach(s => {
+                    s.offset.x = x - s.x
+                    s.offset.y = y - s.y
+                })
+            }
+            return // return if mouse is inside the gates included in selector
+        }
+    }
+
+    // reset -> if the selectedArray is not empty and the mouse was not inside any of its gates
+    if (!e.ctrlKey && selectedArray.length != 0) {
+        selectedArray.forEach(selected => selected.border = false)
+        selectedArray = []
+    }
+
+    let selectedIndex = null
+    // setting offset for single gates selected
+    for (let i = gates.length - 1; i >= 0; i--) {
+        // check whether the mouse is withing a gate
+        let mouseInsideGate = gates[i].checkMouse(x - translate.x, y - translate.y)
+
+        if (mouseInsideGate) { // for single movement of 
+            gates[i].offset.x = gates[i].x - x
+            gates[i].offset.y = gates[i].y - y
+            gates[i].border = true
+            selectedArray.push(gates[i])
+            selectedIndex = i
+            break
+        }
+    }
+
+    // delete the selected from the array and push it at the end
+    // if (selectedIndex != null) {
+    //     gates.splice(selectedIndex, 1)
+    //     gates.push(selectedArray[0])
+    // }
+    
+})
+
+canvas.addEventListener('mousemove', (e) => {
+    // animate() // rerender everytime the mouse moves
+    let x = e.clientX
+    let y = e.clientY
+    
+    // some bug here
+    if (mousePressed && e.ctrlKey) { // first priority given to move canvas + ctrl
+        translate.x += x - prev.x
+        translate.y += y - prev.y
+        prev = {x, y}
+        canvas.style.cursor = 'grabbing'
+    } 
+    else if (mousePressed && selectedArray.length != 0) { // second priority given to move gates
+        canvas.style.cursor = 'grabbing'
+        selectedArray.forEach(selected => {
+            selected.x = x - selected.offset.x
+            selected.y = y - selected.offset.y
+        })
+    }
+    else if (mousePressed) {
+        selector.width = x - selector.x
+        selector.height = y - selector.y
+    }
+})
+
+canvas.addEventListener('mouseup', (e) => {
+    // animate() // rerender everytime the mouse is up
+    let x = e.clientX
+    let y = e.clientY
+    mousePressed = false
+    canvas.style.cursor = 'default'
+
+    for (let i = gates.length - 1; i >= 0; i--) {
+        // check whether the gate is withing a selector
+        let gateInsideSelector = gates[i].checkSelector(selector)
+        if (gateInsideSelector) {
+            gates[i].border = true
+            selectedArray.push(gates[i])
+        }
+    }
+
+    selector = {x: 0, y: 0, width: 0, height: 0}
+})
+
+
+window.addEventListener('wheel', (e) => {
+    let x = e.deltaX
+    let y = e.deltaY
+    translate.x -= x
+    translate.y -= y
+})
+
+canvas.addEventListener('contextmenu', (e) => {
+    e.preventDefault()
+    showContextMenu({ x: e.clientX, y: e.clientY })
+})
+
+canvas.addEventListener('click', () => {
+    hideContextMenu()
+})
+
+
+window.addEventListener('keydown', (e) => {
+    if (e.ctrlKey && e.key == 'a') {
+        selectedArray = [...gates]
+        selectedArray.forEach(selected => selected.border = true)
+    }
+    if (e.key == 'Escape') {
+        selectedArray.forEach(selected => selected.border = false)
+        selectedArray = [] 
+    }
+    if (e.keyCode == 37) addKeysToSelectedGates('LEFT')
+    if (e.keyCode == 39) addKeysToSelectedGates('RIGHT')
+    if (e.keyCode == 38) addKeysToSelectedGates('UP')
+    if (e.keyCode == 40) addKeysToSelectedGates('DOWN')
+})
+window.addEventListener('keyup', (e) => {
+    if (e.keyCode == 37) removeKeysToSelectedGates('LEFT')
+    if (e.keyCode == 39) removeKeysToSelectedGates('RIGHT')
+    if (e.keyCode == 38) removeKeysToSelectedGates('UP')
+    if (e.keyCode == 40) removeKeysToSelectedGates('DOWN')
+})
+
+const addKeysToSelectedGates = (key) => selectedArray.forEach(selected => selected.keys.add(key))
+const removeKeysToSelectedGates = (key) => selectedArray.forEach(selected => selected.keys.delete(key))
+
+
+function showContextMenu(pos) {
+    contextMenu.style.left = pos.x + 'px'
+    contextMenu.style.display = 'block'
+
+    let bottomY = pos.y + contextMenu.getBoundingClientRect().height
+    let rightX = pos.x + contextMenu.getBoundingClientRect().width
+    let bottomCondition = bottomY > window.innerHeight
+    let rightCondition = rightX > window.innerWidth
+
+    // checking the condition whether the contextMenu is beyond height and beyond width of the screen
+    contextMenu.style.top = bottomCondition? 'unset': pos.y + 'px'
+    contextMenu.style.bottom = bottomCondition? (window.innerHeight - pos.y) + 'px' : 'unset'
+    contextMenu.style.left = rightCondition? 'unset': pos.x + 'px'
+    contextMenu.style.right = rightCondition? (window.innerWidth - pos.x) + 'px' : 'unset'
+}
+
+function hideContextMenu() {
+    contextMenu.style.display = 'none'
+}
+
+
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.save()
+    ctx.scale(window.devicePixelRatio*scaleFactor, window.devicePixelRatio*scaleFactor)
+    ctx.translate(translate.x, translate.y)
+
+    // drawing all the gates in the gates list
+    gates.forEach(gate => {
+        gate.update()
+        gate.draw(ctx)
+    })
+
+    if (selector.width != 0 && selector.height != 0) {
+        ctx.strokeStyle = '#6965db'
+        ctx.strokeRect(
+            selector.x - translate.x, 
+            selector.y - translate.y, 
+            selector.width, 
+            selector.height)
+    }
+
+
+    ctx.restore()
+    requestAnimationFrame(animate)
+}
+animate()
